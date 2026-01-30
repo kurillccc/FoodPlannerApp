b5c0b55321c6b158ea380a77ca512c1e2cfafc24
Кирилл Чернов <89049125360@mail.ru>
2026-01-30 17:40:13 +0300

    Реализовал шаблон маршрутизатора и настроил навигацию

--- FoodPlanner/Features/Router/Router.swift
+++ FoodPlanner/Features/Router/Router.swift
@@ -5,3 +5,22 @@
 //  Created by Кирилл on 30.01.2026.
 //
 
+import UIKit
+
+final class Router {
+    
+    let navigation: UINavigationController
+        
+    init(navigation: UINavigationController) {
+        self.navigation = navigation
+    }
+    
+    private func push(controller: UIViewController) {
+        navigation.pushViewController(controller, animated: true)
+    }
+    
+    private func pop() {
+        navigation.popViewController(animated: true)
+    }
+    
+}
--- FoodPlanner/Features/View/Controllers/WelcomeViewController.swift
+++ FoodPlanner/Features/View/Controllers/WelcomeViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    var router: Router
+    
+    init(router: Router) {
+        self.router = router.super.init(nibName: nil, bundle: nil)
+    }
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

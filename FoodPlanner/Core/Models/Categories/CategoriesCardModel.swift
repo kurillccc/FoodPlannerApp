91c11688d3e6a6458f709d18309a7bd8e5fcbfd5
Кирилл Чернов <89049125360@mail.ru>
2026-02-02 11:05:59 +0300

    Добавил id категорий в model

---
+++ FoodPlanner/Core/Models/Categories/CategoriesCardModel.swift
@@ -0,0 +1,16 @@
+//
+//  CategoriesCardModel.swift
+//  FoodPlanner
+//
+//  Created by Кирилл on 01.02.2026.
+//
+
+import UIKit
+
+struct CategoriesCardModel {
+    let categoryId: String
+    let title: String
+    let image: UIImage
+    let color: UIColor
+    let borderColor: UIColor
+}
---
+++ FoodPlanner/Core/Models/Categories/CategoriesViewMode.swift
@@ -0,0 +1,111 @@
+//
+//  CategoriesViewMode.swift
+//  FoodPlanner
+//
+//  Created by Кирилл on 01.02.2026.
+//
+
+import UIKit
+
+struct CategoriesViewModel {
+    
+    let categories: [CategoriesCardModel]
+    
+    init() {
+        self.categories = [
+            CategoriesCardModel(
+                categoryId: "bakery",
+                title: "Bakery and Snacks",
+                image: Images.Category.bakery,
+                color: AppColor.Card.Bakery.fill,
+                borderColor: AppColor.Card.Bakery.stroke
+            ),
+            CategoriesCardModel(
+                categoryId: "fruits",
+                title: "Frash Fruits & Vegetable",
+                image: Images.Category.fruits,
+                color: AppColor.Card.FruitVeg.fill,
+                borderColor: AppColor.Card.FruitVeg.stroke
+            ),
+            CategoriesCardModel(
+                categoryId: "dairy",
+                title: "Dairy",
+                image: Images.Category.milk,
+                color: AppColor.Card.Dairy.fill,
+                borderColor: AppColor.Card.Dairy.stroke
+            ),
+            CategoriesCardModel(
+                categoryId: "meat",
+                title: "Meat",
+                image: Images.Category.meat,
+                color: AppColor.Card.Meat.fill,
+                borderColor: AppColor.Card.Meat.stroke
+            ),
+            CategoriesCardModel(
+                categoryId: "beverages",
+                title: "Beverages",
+                image: Images.Category.drinks,
+                color: AppColor.Card.Drinks.fill,
+                borderColor: AppColor.Card.Drinks.stroke
+            ),
+            CategoriesCardModel(
+                categoryId: "bakery",
+                title: "Bakery and Snacks",
+                image: Images.Category.bakery,
+                color: AppColor.Card.Bakery.fill,
+                borderColor: AppColor.Card.Bakery.stroke
+            ),
+            CategoriesCardModel(
+                categoryId: "oil",
+                title: "Cooking Oil & Ghee",
+                image: Images.Category.oils,
+                color: AppColor.Card.Oils.fill,
+                borderColor: AppColor.Card.Oils.stroke
+            ),
+            CategoriesCardModel(
+                categoryId: "bakery",
+                title: "Dairy",
+                image: Images.Category.milk,
+                color: AppColor.Card.Dairy.fill,
+                borderColor: AppColor.Card.Dairy.stroke
+            ),
+            CategoriesCardModel(
+                categoryId: "meat",
+                title: "Meat",
+                image: Images.Category.meat,
+                color: AppColor.Card.Meat.fill,
+                borderColor: AppColor.Card.Meat.stroke
+            ),
+            CategoriesCardModel(
+                categoryId: "bakery",
+                title: "Bakery and Snacks",
+                image: Images.Category.bakery,
+                color: AppColor.Card.Bakery.fill,
+                borderColor: AppColor.Card.Bakery.stroke
+            ),
+            CategoriesCardModel(
+                categoryId: "beverages",
+                title: "Beverages",
+                image: Images.Category.drinks,
+                color: AppColor.Card.Drinks.fill,
+                borderColor: AppColor.Card.Drinks.stroke
+            ),
+            CategoriesCardModel(
+                categoryId: "bakery",
+                title: "Bakery and Snacks",
+                image: Images.Category.bakery,
+                color: AppColor.Card.Bakery.fill,
+                borderColor: AppColor.Card.Bakery.stroke
+            )
+        ]
+        
+    }
+    
+    var numberOfCategories: Int {
+        categories.count
+    }
+    
+    func category(at index: Int) -> CategoriesCardModel {
+        categories[index]
+    }
+}
